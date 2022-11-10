031f315ceae32433e93d2a15b5ee2690f9debc56
Tim <timwagner997@gmail.com>
2022-11-10 14:54:54 +0100

    first commit

---
+++ desktop/src/micromacrocrimedetectives/micromacrospaceship/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package micromacrocrimedetectives.micromacrospaceship;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import micromacrocrimedetectives.micromacrospaceship.MicroMacroGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("MicroMacroSpaceShip");
+		new Lwjgl3Application(new MicroMacroGame(), config);
+	}
+}
